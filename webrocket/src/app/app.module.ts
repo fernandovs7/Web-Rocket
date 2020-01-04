2e9d4263a59b5655be8dbbcc93b23768c1b3ba6d
alovarga <alovarga@publicisgroupe.net>
2020-01-03 20:44:16 -0600

    Bootstrap added to the project

--- webrocket/src/app/app.module.ts
+++ webrocket/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MybootstrapModule } from './mybootstrap/mybootstrap.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MybootstrapModule
   ],
   providers: [],
   bootstrap: [AppComponent]
